7cae3618b2ed3ffcf9b71e5abc34ad00a21e6987
Jairo Rotava <jairo.rotava@gmail.com>
2022-09-09 18:26:15 -0300

    Pre processamento de arquivos embrapa para dataframe

--- embrapa.R
+++ embrapa.R
@@ -2,7 +2,10 @@ library(stringi)
 
 read_embrapa <- function(x) {
   data <- read.csv(x, sep=";", quote="\"", 
-                   header = TRUE, skip = 2, fileEncoding="latin1")
+                   header = TRUE, skip = 2, fileEncoding="utf-8")
+  
+  #data <- read.csv(x, sep=";", quote="\"", 
+  #                 header = TRUE, skip = 2, fileEncoding="latin1")
   
   # Faz limpeza do cabecalho
   # Cabecalho nao pode comecar com numero, por isso adiciona um x na frente.
@@ -19,3 +22,24 @@ read_embrapa <- function(x) {
   
   return(data)
 }
+
+get_header_embrapa <- function(X) {
+  df <- read_embrapa(X)
+  return(colnames(df))
+}
+
+
+read_all_embrapa <- function(x) {
+  files_embrapa = list.files(path=x, pattern=NULL, all.files=FALSE,
+                             full.names=TRUE)
+  df_appended = data.frame()
+  
+  for(file_name in files_embrapa) {
+    print(paste("Reading file:",toString(file_name)))
+    df <- read_embrapa(file_name)
+    df_appended <- rbind(df_appended, df)
+  }
+  return(df_appended)
+
+}
+
